8908b6e3112e2f6451b3e0505727a327db4047d7
BKhurshid <josephsun4@gmail.com>
2015-11-11 19:36:48 -0800

    Just adding for testing the google api

---
+++ angularModuleSampleUntilRJFinishes.js
@@ -0,0 +1,54 @@
+/*
+Optional section if you've completed the rest of this prompt:   
+  * If you are using ng-view, or some other directives, Angular requires you serve your code from a web server, not directly from the file-system. One simple way to do this is to run `python -m SimpleHTTPServer` from the directory where your code is. Going to the url the program's output gives you will allow you to open your files in the browser from the local python web server. If you don't have python installed, you'll first need to run `brew install python`.
+*/
+
+
+/*
+
+*/
+
+angular.module("Compare",  ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/index');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/index',
+            controller: 'GetDataApi'
+            // templateUrl: 'index.html'
+        });
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        
+        
+})
+        .controller('GetDataApi', ['$rootScope','$scope', '$http', functionName]);
+
+    function functionName($rootScope,$scope, $http ){
+        console.log("Hello");
+
+        function fetch() {
+        $http.get(" http://data.colorado.gov/resource/4ykn-tg5h.json")
+          .then(function (response) {
+                console.log("success");
+              console.log("response", response);
+
+              $scope.details = response;
+              $scope.data1 = response.data;
+              $scope.fullData = [];
+              for (var i = 0; i < response.data.length; i++) {
+                $scope.fullData.push(response.data[i].agentfirstname + " " + response.data[i].agentlastname + " is the principal agent for the coporation " + response.data[i].entityname + ". This corporation is located in Colorado. The address of the corporation in question is " + response.data[i].principaladdress1+ ", Colorado, United States. The Zip code of which is " + response.data[i].principalzipcode + ".");
+              }
+            console.log("data1", response.data.length);
+            console.log("data1", $scope.data1);
+          }, function(error){
+                  console.log("error message:", error);
+        });
+      }
+ 
+  fetch(); 
+}
